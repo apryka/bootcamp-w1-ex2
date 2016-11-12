36fd48ce5df487ba77c84a5209424319aeaf018c
apryka <artur.pryka@gmail.com>
2016-11-12 23:57:20 +0100

    selectors added

---
+++ js/main.js
@@ -0,0 +1,16 @@
+/* selectors */
+
+/* 1 */
+$('div.grid.grid-12');
+
+/* 2 */
+$('nav').find('a[href^="http"]');
+
+/* 3 */
+$('input:radio, input:checkbox').not(':checked');
+
+/* 4 */
+$('div#text p:empty').first();
+
+/* 5 */
+$('.pagination-item').not('span');
